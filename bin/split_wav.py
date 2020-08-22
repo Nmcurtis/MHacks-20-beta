2b7ffd1154702ff0f3e936e362d31ac97fa8c5b4
Nicholas Curtis <NickCurtisDev@gmail.com>
2020-08-22 18:21:53 -0400

    Moved split_wav.py to bin

---
+++ bin/split_wav.py
@@ -0,0 +1,42 @@
+from pydub import AudioSegment
+
+
+def time_stamp_to_seconds_converter(timestamp: str):  # m:s
+    timestamp.split(":")
+    return int(timestamp[0]) * 60 + int(timestamp[1])
+
+
+def seconds_to_timestamp_converter(seconds: int):
+    minutes = seconds // 60
+    sec = seconds % 60
+    return str(minutes) + ":" + str(sec)
+
+
+def save_a_wav_segment(t1: int, t2: int, wav_file_path):  # times should be in seconds
+    t1 = t1 * 1000  # Works in milliseconds
+    t2 = t2 * 1000
+    new_audio = AudioSegment.from_wav(wav_file_path) # should be in wav_files dir/other known path
+    new_audio = new_audio[t1:t2]
+    identifier = str(t1) + "_" + str(t2)
+    file_name_parts = wav_file_path.split('.')
+    new_file_name = file_name_parts[0] + identifier + file_name_parts[1]
+    new_audio.export(new_file_name, format="wav")  # Exports to a wav file in the current path.
+
+
+def split_all_audio_into_segments(wav_file_path, segment_length):  # files should be from a known path, seg_length in seconds
+    audio_length_in_seconds = wav_file_path.duration_seconds
+    start_time = 0
+    end_time = segment_length
+    while True:
+        if end_time > audio_length_in_seconds:
+            end_time = audio_length_in_seconds
+            save_a_wav_segment(start_time, end_time, wav_file_path)
+            break
+        save_a_wav_segment(start_time, end_time, wav_file_path)
+        start_time = end_time
+        end_time += segment_length
+
+
+
+
+
