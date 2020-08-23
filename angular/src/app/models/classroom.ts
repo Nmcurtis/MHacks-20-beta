509a21fc237f03a1f5f80e2e0b7f369e5ff24619
kennedysean <kenne476@msu.edu>
2020-08-23 02:53:16 -0400

    Add image previews

--- angular/src/app/models/classroom.ts
+++ angular/src/app/models/classroom.ts
@@ -3,10 +3,12 @@ import { Lecture } from './lecture';
 export class Classroom {
     classCode: string;
     className: string;
+    preview: string;
     lectures: Lecture[] = [];
     
-    constructor(code: string, name: string) {
+    constructor(code: string, name: string, preview: string) {
         this.classCode = code;
         this.className = name;
+        this.preview = preview;
     }
 }
--- angular/src/app/models/lecture.ts
+++ angular/src/app/models/lecture.ts
@@ -1,9 +1,12 @@
 export class Lecture {
     date: Date;
     title: string;
+    preview: string;
 
-    constructor(date: Date, title: string) {
+
+    constructor(date: Date, title: string, preview: string) {
         this.date = date;
         this.title = title;
+        this.preview = preview;
     }
 }
