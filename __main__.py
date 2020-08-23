fe25291f91445edaaebb0a5bd61ca21cf6f8a595
deysonali <39059765+deysonali@users.noreply.github.com>
2020-08-22 22:53:09 -0400

    adds classes and videos

--- __main__.py
+++ __main__.py
@@ -4,3 +4,28 @@ import app
 webapp = app.create_app()
 web.run_app(webapp)
 
+# import json
+#
+# a_0 = {"id": 0, "title": "Why So Many People Want to be Writers", "url": "https://youtu.be/axXn_Vn2vYo"}
+# a_1 = {"id": 1, "title": "How to Write Descriptively", "url": "https://youtu.be/RSoRzTtwgP4"}
+# a_2 = {"id": 2, "title": "How to Build a Fictional World", "url": "https://youtu.be/ZQTQSbjecLg"}
+#
+# b_0 = {"id": 0, "title": "Why Study the Government?", "url": "https://youtu.be/lrk4oY7UxpQ"}
+# b_1 = {"id": 1, "title": "The Bicameral Congress", "url": "https://youtu.be/n9defOwVWS8"}
+# b_2 = {"id": 2, "title": "Congressional Elections", "url": "https://youtu.be/qxiD9AEX4Hc"}
+#
+# c_0 = {"id": 0, "title": "Early Programming", "url": "https://youtu.be/nwDq4adJwzM"}
+# # c_1 = {"id": 1, "title": "", "url": ""}
+# # c_2 = {"id": 2, "title": "a lesson", "url": "a url"}
+#
+# classes = {"College Writing 101": {"Lecture 1": a_0, "Lecture 2": a_1, "Lecture 3": a_2},
+#            "Political Science 227": {"Lecture 1": b_0, "Lecture 2": b_1, "Lecture 3": b_2}}
+# print(json.dumps(classes))
+# f = open("classroom_video_urls.txt", "w")
+# f.write(json.dumps(classes))
+# f.close()
+# f = open("classroom_video_urls.txt", "r")
+# content = f.read()
+#
+# print(json.loads(content))
+# print(json.loads(content)["Political Science 227"]['Lecture 1'])
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 from aiohttp import web
 from google.cloud import storage
 
+
 class ClassroomsApp:
     def __init__(self, client):  # classroom list - list of classroom objects
         self.client = client
@@ -29,26 +30,34 @@ class ClassroomsApp:
         else:
             print("Not a legit classroom bucket")
 
-    async def get_a_classroom_video(self, req):
+    async def get_a_classroom_video_url(self, req):
         bucket_name = req.match_info.get('classroom_name')
         video_number = req.match_info.get('video_number')
-        classroom_names = json.loads(self.get_classroom_names())
+        classrooms = self.client.list_buckets()
+        classroom_names = [classroom.name for classroom in classrooms]
         if bucket_name in classroom_names:
             classroom_bucket = self.client.get_bucket(bucket_name)
             blobs = classroom_bucket.list_blobs()
-            classroom_videos = [blob.name for blob in blobs]
-            requested_file = classroom_videos[int(video_number)]
-            return web.FileResponse()
+            classroom_video_links = [blob.name for blob in blobs]
+            requested_video = classroom_video_links[int(video_number)]
+            return web.Response(text=requested_video)
         else:
             print("Not a legit classroom bucket")
 
     async def search_video(self, req): # send search term to function?
-        search_string = req.query['search_term']
-        return 0
+        class_name = req.match_info.get('classroom_name')
+        lec_num = req.match_info.get('video_number')
+        #search_string = req.match_info.get('search_term')
+        #timestamp = theProcessingFunction(classroom_name, video_number, search_string)
+
+        f = open("classroom_video_urls.txt", "r")
+        content = f.read()
+        content = json.loads(content)
+        url = content["Poliical Science 227"]['Lecture 1']
+        return web.Response(text=url)
 
     async def root_index(self, _):
-        return web.Response(text="testing")
-        #return web.FileResponse("index.html")
+        return web.FileResponse("angular/src/index.html")
 
 
 async def on_prepare(_, response):
@@ -65,10 +74,10 @@ def create_app():
                     web.get('/classrooms', app.classroom_service.get_classroom_names),
                     web.get('/classrooms/{classroom_name}', app.classroom_service.get_classroom_video_names),
                     web.get('/classrooms/{classroom_name}/{video_number}',
-                            app.classroom_service.get_a_classroom_video),
+                            app.classroom_service.get_a_classroom_video_url),
                     web.post('/classrooms/{classroom_name}/{video_number}/{search_term}',
                              app.classroom_service.search_video),
-                    #web.static('/', ),
+                    web.static('/', ""),
                     # This must be the last route
                     ])
     return app
