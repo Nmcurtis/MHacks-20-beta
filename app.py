8ea7f43b03fff0ee17706ccba22b1c34df4a53a8
deysonali <39059765+deysonali@users.noreply.github.com>
2020-08-23 00:22:23 -0400

    fixes CORS issue

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 from aiohttp import web
 from google.cloud import storage
-
+import aiohttp_cors
 
 class ClassroomsApp:
     def __init__(self, client):  # classroom list - list of classroom objects
@@ -33,16 +33,21 @@ class ClassroomsApp:
     async def get_a_classroom_video_url(self, req):
         bucket_name = req.match_info.get('classroom_name')
         video_number = req.match_info.get('video_number')
-        classrooms = self.client.list_buckets()
-        classroom_names = [classroom.name for classroom in classrooms]
-        if bucket_name in classroom_names:
-            classroom_bucket = self.client.get_bucket(bucket_name)
-            blobs = classroom_bucket.list_blobs()
-            classroom_video_links = [blob.name for blob in blobs]
-            requested_video = classroom_video_links[int(video_number)]
-            return web.Response(text=requested_video)
-        else:
-            print("Not a legit classroom bucket")
+        # classrooms = self.client.list_buckets()
+        # classroom_names = [classroom.name for classroom in classrooms]
+        # if bucket_name in classroom_names:
+        #     classroom_bucket = self.client.get_bucket(bucket_name)
+        #     blobs = classroom_bucket.list_blobs()
+        #     classroom_video_links = [blob.name for blob in blobs]
+        #     requested_video = classroom_video_links[int(video_number)]
+        #     return web.Response(text=requested_video)
+        # else:
+        #     print("Not a legit classroom bucket")
+        f = open("classroom_video_urls.txt", "r")
+        content = f.read()
+        content = json.loads(content)
+        url = {"url": content['Political Science 227']['Lecture 1']}
+        return web.json_response(url)
 
     async def search_video(self, req): # send search term to function?
         class_name = req.match_info.get('classroom_name')
@@ -53,8 +58,8 @@ class ClassroomsApp:
         f = open("classroom_video_urls.txt", "r")
         content = f.read()
         content = json.loads(content)
-        url = content["Poliical Science 227"]['Lecture 1']
-        return web.Response(text=url)
+        url = {"url": "content['Political Science 227']['Lecture 1']"}
+        return web.json_response(json.dumps(url))
 
     async def root_index(self, _):
         return web.FileResponse("angular/src/index.html")
@@ -69,15 +74,30 @@ def create_app():
     app.on_response_prepare.append(on_prepare)
     app.classroom_service = ClassroomsApp(
         client=storage.Client.from_service_account_json('C:\\Users\\Sonali\\Downloads\\classroom-test-ffd25d8801b2.json'))
+
+
     app.add_routes([web.get('/', app.classroom_service.root_index),
                     web.get('/fail', app.classroom_service.fail_route),
                     web.get('/classrooms', app.classroom_service.get_classroom_names),
                     web.get('/classrooms/{classroom_name}', app.classroom_service.get_classroom_video_names),
                     web.get('/classrooms/{classroom_name}/{video_number}',
                             app.classroom_service.get_a_classroom_video_url),
-                    web.post('/classrooms/{classroom_name}/{video_number}/{search_term}',
+                    web.get('/classrooms/{classroom_name}/{video_number}/{search_term}',
                              app.classroom_service.search_video),
-                    web.static('/', ""),
                     # This must be the last route
                     ])
+
+    # Configure default CORS settings.
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+    })
+
+    # Configure CORS on all routes.
+    for route in list(app.router.routes()):
+        cors.add(route)
+
     return app
